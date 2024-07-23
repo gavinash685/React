57e0ea23a74005f49a60f5437dee044633f26255
gavinash <avinashgaikwad685@gmail.com>
2024-07-23 13:16:49 +0530

    first project commit

---
+++ 01firstproject/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0);  
+  const increaseValue = ()=>{
+    counter++;
+    if(counter > 20 ) {
+      counter = 20
+    }
+    setCounter(counter);
+  };
+  const decreseValue = () =>{
+    counter--;
+    if(counter < 0 ){
+      counter = 0
+    }
+    setCounter(counter);
+  }
+  return (
+    <>
+      <h1>Counter value is { counter }</h1>
+      <button onClick={increaseValue}>Add value</button><br />
+      <button onClick={decreseValue}>Decrease value</button>
+    </>
+  )
+}
+
+export default App
